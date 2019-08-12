fda9c20a18d38ec067b93b5457b5ea017a8e0de8
Arnon2BDEV <Arnon2BDEV@users.noreply.github.com>
2019-08-12 19:57:23 +0700

    Update Script

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Playermodel
+{
+    [System.Serializable]
+    public class Player
+    {
+        public string id;
+        public string name;
+        public int status;
+        public string inroomname;
+    }
+}
---
+++ Assets/QuestionModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class QuestionModel
+{
+    public int id;
+    public string question;
+    public List<Choices> choices;
+    public string answer;
+
+    [System.Serializable]
+    public class Choices
+    {
+        public int id;
+        public string text;
+        public bool value;
+    }
+}
+
